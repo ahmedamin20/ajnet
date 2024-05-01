0aacac9337a551efd5fd4998dbe65081197e7255
AhmedAmin <ahmedamin01147@hotmail.com>
2024-05-02 01:44:38 +0300

    update

--- scraping.py
+++ scraping.py
@@ -12,8 +12,8 @@ def get_html(url):
     response = requests.get(url).text
     soup = BeautifulSoup(response, 'lxml')
     data =  get_data(soup)
-    with open('data.json', 'w') as f:
-        json.dump(data, f)
+    with open('data.json', 'w', encoding='utf-8') as f:
+        json.dump(data, f, indent=4, ensure_ascii=False)
 
     return None
 
@@ -32,11 +32,9 @@ def get_data(soup):
             
             main_page = None
             if page_link is not None:
-                page_req_link = page_link.get('href')
-                page_data = requests.get(base_url+page_req_link)
-                main_page = BeautifulSoup(page_data.content,'lxml')
-                title = main_page.find("h1")
-                article = main_page.find("div",{"class":"wysiwyg--all-content"}).find_all("p")
+                main_page = page_link.get('href')
+            #     article = main_page.find("div",{"class":"wysiwyg--all-content"}).find_all("p")
+            
             a_tag = None
             if label is not None:
                 a_tag = label.find("a").text.strip()
@@ -45,9 +43,26 @@ def get_data(soup):
             x = {
                     "title": a_tag,
                     "image": base_url+image_src,
-                    "article": ''.join([p.text for p in article])
+                    # "article": ''.join([p.text for p in article]),
+                    "link": main_page
                 }
             data.append(x)
     return data
+
+
+def get_page (page_link) :
+    response = requests.get(page_link)
+    main_page = BeautifulSoup(response.content, 'html.parser')
+    article_tag = main_page.find("div",{"class":"wysiwyg--all-content"}).find_all("p")
+    article = ''.join([p.text for p in article_tag])
+    data = {
+        "article": article,
+    }
+    with open('test.json', 'w', encoding='utf-8') as f:
+        json.dump(data, f, indent=4, ensure_ascii=False)
+    return None
+
+
+get_page("https://www.ajnet.me/news/2024/5/1/%D8%A3%D8%A8%D8%B1%D8%B2-%D8%AA%D8%B7%D9%88%D8%B1%D8%A7%D8%AA-%D8%A7%D9%84%D9%8A%D9%88%D9%85-%D8%A7%D9%84%D9%80208-%D9%85%D9%86-%D8%A7%D9%84%D8%AD%D8%B1%D8%A8")
 
 get_html("https://www.ajnet.me/where/arab/palestine/")
